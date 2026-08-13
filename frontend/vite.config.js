115e6fa6a86849159702ca737222970b0e2171aa
JoaoGabrielDeSouzaPenedo <SEU_EMAIL_DO_GITHUB>
2026-08-13 17:44:12 -0300

    parte do back 2

---
+++ frontend/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+
+    allowedHosts: [
+      '.trycloudflare.com'
+    ],
+
+    proxy: {
+
+      '/auth': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+
+    }
+  }
+})
